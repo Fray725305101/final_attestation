8fc22243f54ec685b85d7df21692a2ea3d84199f
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-09-25 16:40:25 +0300

    Пишем схему БД

--- src/main/resources/db/migration/schema.sql
+++ src/main/resources/db/migration/schema.sql
@@ -1,8 +1,9 @@
+--Здесь
 --Создаём таблицу товаров
 create table if not exists product (
     id serial primary key,
-    product_name varchar(100) not null,
-    price decimal(10,2) not null check (price >= 0),
+    product_name varchar(1000) not null,
+    price decimal(10,2) not null check (price > 0),
     quantity integer not null check (quantity >= 0),
     category varchar(100) not null
 );
@@ -10,9 +11,16 @@ create table if not exists product (
 --Создаём таблицу покупателей
 create table if not exists customer (
     id serial primary key,
-    customer_name varchar(1000) not null,
+    first_name varchar(100),
+    last_name varchar(100),
     phone varchar(20) not null,
-    email varchar(300)
+    email varchar(300),
+
+    --Проверяем, что заполнены хотя бы имя или фамилия
+    check (
+        (first_name is not null and first_name != '') or
+        (last_name is not null and last_name != '')
+    )
 );
 
 --Создаём таблицу статусов заказов
@@ -30,4 +38,16 @@ create table if not exists order_head (
 
     foreign key (customer_id) references customer(id),
     foreign key (status_id) references status(id)
+);
+
+--Создаём таблицу тела заказов
+create table if not exists order_body (
+    id serial primary key,
+    head_id integer not null,
+    product_id integer not null,
+    quantity integer not null check (quantity > 0),
+    price decimal(10,2) not null check (price > 0),
+
+    foreign key (head_id) references order_head(id),
+    foreign key (product_id) references product(id)
 );
