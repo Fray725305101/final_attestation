68bb74253e751dfd8e04ce3252c251b8f012de01
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-09-25 17:56:18 +0300

    Подготавливаем данные для таблиц

--- src/main/resources/db/migration/data.sql
+++ src/main/resources/db/migration/data.sql
@@ -1,4 +1,23 @@
-insert into order_status (status_name) values ('Создан'),
+insert into categories (category_name) values
+('ОЗУ'),
+('ЦПУ'),
+('Накопитель'),
+('Блок питания');
+
+insert into order_status (status_name) values
+('Создан'),
 ('В работе'),
 ('Готов к выдаче'),
 ('Выдан');
+
+insert into customers (first_name, last_name, phone, email) values
+('Йо', 'Асакура', '+1234567890','shaman@king.org'),
+('Амидамару', '', '+1234567890', ''),
+('Финн', 'Парнишка', '+2345678901', 'finn.human@adventuretime.com'),
+('Джейк', 'Пёс', '+3456789012', 'jake.dog@adventuretime.com'),
+('Морти', 'Смит', '+4567890123', 'morty.smith@rickandmorty.com'),
+('Рик', 'Санчез', '+00000000000', 'wubbalubbadubdub@rickandmorty.com'),
+('Скуби', 'Ду', '+5678901234', 'scooby.doo@hb.com'),
+('Питер', 'Паркер', '+6789012345', 'your_friendly_neighbor@marvel.com'),
+('Саске', 'Учиха', '+7890123456', 'sasuke.uchiha@konoha.com'),
+('Багз', 'Банни', '+8901234567', 'bugs.bunny@wb.com')
