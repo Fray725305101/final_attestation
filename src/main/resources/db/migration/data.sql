888f49ac503836f2a670ee5587d6d7253945a705
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-09-26 12:51:46 +0300

    Продолжаем заполнять данными

--- src/main/resources/db/migration/data.sql
+++ src/main/resources/db/migration/data.sql
@@ -20,4 +20,17 @@ insert into customer (first_name, last_name, phone, email) values
 ('Скуби', 'Ду', '+5678901234', 'scooby.doo@hb.com'),
 ('Питер', 'Паркер', '+6789012345', 'your_friendly_neighbor@marvel.com'),
 ('Саске', 'Учиха', '+7890123456', 'sasuke.uchiha@konoha.com'),
-('Багз', 'Банни', '+8901234567', 'bugs.bunny@wb.com')
+('Мег', 'Гриффин', '+8901234567', 'megatron@familyguy.com');
+
+insert into product (product_name, price, quantity, category_id) values
+('DDR4 16GB 3200MHz', 4500.00, 25, 1),
+('DDR5 32GB 4800MHz', 8900.00, 15, 1),
+('Intel Core i7-13700K', 32000.00, 8, 2),
+('AMD Ryzen 7 7800X3D', 28500.00, 10, 2),
+('SSD NVMe 1TB', 5500.00, 30, 3),
+('SSD SATA 512GB', 3200.00, 40, 3),
+('HDD 2TB 7200RPM', 4500.00, 20, 3),
+('Be Quiet! 750W 80+ Gold', 8900.00, 12, 4),
+('Seasonic 850W 80+ Platinum', 12500.00, 8, 4),
+('DDR4 8GB 2666MHz', 2500.00, 35, 1);
+
