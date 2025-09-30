1392eef8b05dd294d4aba43fa7384914c8003f70
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-09-30 15:08:05 +0300

    Пишем запросы и редачим данные для красоты

--- src/main/resources/db/queries/test-queries.sql
+++ src/main/resources/db/queries/test-queries.sql
@@ -23,3 +23,24 @@ join final_attestation_pakudin.order_head oh on os.id = oh.status_id
 group by os.id
 having count(*) > 2
 order by os.id asc;
+
+update final_attestation_pakudin.product
+set price = price * 1.10
+where category_id = 1;
+
+update final_attestation_pakudin.order_head
+set status_id = 4
+where customer_id in (
+    select id from final_attestation_pakudin.customer
+    where last_name like 'Grif%'
+    );
+
+update final_attestation_pakudin.product
+set quantity = quantity - ordered.ordered_quantity
+from (
+    select product_id, quantity as ordered_quantity
+    from final_attestation_pakudin.order_body
+    where head_id = 1
+) as ordered
+where final_attestation_pakudin.product.id = ordered.product_id
+and final_attestation_pakudin.product.quantity >= ordered.ordered_quantity;
